bc7b5e6aa0636fb21ad7997e4991221a84af6210
Jane Zhu <jane@MacBook-Pro.local>
2020-08-24 02:16:31 -0400

    Updates

---
+++ precache-manifest.01445f006fe57bf6351111a1e17975a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb112a830bb0562b6bc85e9a0f4b89e5",
+    "url": "/Marlabs-UI-HW14/index.html"
+  },
+  {
+    "revision": "47642a1a4bedbdd15309",
+    "url": "/Marlabs-UI-HW14/static/css/main.5cc69460.chunk.css"
+  },
+  {
+    "revision": "6475f4a6bb25bd607e87",
+    "url": "/Marlabs-UI-HW14/static/js/2.f45d9cb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Marlabs-UI-HW14/static/js/2.f45d9cb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47642a1a4bedbdd15309",
+    "url": "/Marlabs-UI-HW14/static/js/main.f6378ea9.chunk.js"
+  },
+  {
+    "revision": "3610326abc5db8d08686",
+    "url": "/Marlabs-UI-HW14/static/js/runtime-main.62f53f41.js"
+  }
+]);
